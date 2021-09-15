4385dfe2eaf3a91a884593b6f76089b1eb32e34e
gianluca galota <gianluca.galota@gmail.com>
2021-09-15 16:44:14 +0100

    navbar component created and styled

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+// Styles Components
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+
+import logo from '../../assets/AllAmazingDealsLogo.png'
+
+// Styles
+import useStyle from './styles'
+
+const Navbar = () => {
+  const classes = useStyle();
+
+  return (
+    <>
+      <AppBar position="fixed" className={classes.appBar} color="inherit">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title} color="inherit">
+            <img src={logo} alt="AllAmazingDeals" height="30px" className={classes.image} />
+          </Typography>
+          <div className={classes.grow} />
+          <div className={classes.button}>
+            <IconButton aria-label="Show cart items" color="inherit">
+              <Badge badgeContent={2} color="inherit">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </>
+  )
+}
+
+export default Navbar;
