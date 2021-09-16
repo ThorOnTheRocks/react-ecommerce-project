dd6766b1e89e3875f605f1ae0cfb7532f1ba0ddb
gianluca galota <gianluca.galota@gmail.com>
2021-09-16 13:30:05 +0100

    commerce.js api implemented with a list of products

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { commerce } from "./lib/commerce";
 import Navbar from "./components/Navbar/Navbar";
 import Products from "./components/Products/Products";
 
 const App = () => {
+  const [products, setProducts] = useState([])
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  }
+
+  useEffect(() => {
+    fetchProducts()
+  }, []);
+
+  console.log(products)
+
   return (
     <div>
       <Navbar />
-      <Products />
+      <Products products={products} />
     </div>
   )
 }
